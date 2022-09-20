f9164eb257017efdc62b3de0efbedf09eea58f9d
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-09-20 13:57:38 +0200

    Update func.js

--- func.js
+++ func.js
@@ -19,6 +19,8 @@ process.on('exit', function(code) {
 fdk.handle(async function(input, ctx){
 
   let tableName;
+  let id;
+  let descTable=false;
 
   // Reading parameters from standard input for TEST purposes
   if (input && input.tableName)
@@ -30,24 +32,97 @@ fdk.handle(async function(input, ctx){
         var adr = hctx.requestURL;
         var q = url.parse(adr, true);
         tableName = q.pathname.split('/')[2]
+        id = q.pathname.split('/')[3]
+        if (id && id == 'desc')
+           descTable=true;
+        method = hctx.method
+        body = ctx.body
   }
-  //retrieving the request headers
-  headers = ctx.Headers
-  config  = ctx.Config
-  method = ctx.Method
-  
-  return ctx;
-
   
   if ( !client ) {
     client = createClientResource();
   }
   
-  rows = executeQuery(`SELECT * FROM ${tableName} LIMIT ${lim}`);
+  rows = getAllRecords(tableName, q);
 
   return rows;
 
 }, {});
+
+// Show the structure of the table tablename
+
+async function descTable (tablename) {
+   try {
+      let resExistingTab = await client.getTable(tablename);
+      await client.forCompletion(resExistingTab);
+      return resExistingTab
+    } catch (err){
+        console.error('failed to show tables', err);
+        return { error: err };
+    } finally {
+    }
+  });
+
+
+// Create a new record in the table tablename
+async function createRecord (tablename, record) {
+    try {
+        const result = await client.put(tablename, record, {exactMatch:true} );
+        res.json({ result: result});
+    } catch (err) {
+        console.error('failed to insert data', err);
+        return { error: err };
+    }
+});
+
+// Get a record from the table tablename by id
+// Currently the id is hardcoded as key of the table
+async function getRecord (tablename, id) {
+    try {
+        const result = await client.get(tablename, { id })
+        res.json(result.row);
+    } catch (err) {
+        console.error('failed to get data', err);
+        return { error: err };
+    }
+});
+
+// Delete a record from the table tablename by id
+// Currently the id is hardcoded as key of the table
+async function deleteRecord (tablename, id) {
+    try {
+        const result = await client.delete(tablename, { id });
+        res.json({ result: result});
+    } catch (err) {
+        console.error('failed to delete data', err);
+        return { error: err };
+    }
+});
+
+// Get all records for the table tablename
+async function getAllRecords (tablename, req) {
+    let statement = "SELECT * FROM " + tablename;
+    const rows = [];
+    let offset;
+
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const orderby = req.query.orderby;
+    if (page)
+      console.log (page)
+    if (orderby )
+      statement = statement + " ORDER BY " + orderby;
+    if (limit)
+      statement = statement + " LIMIT " + limit;
+    if (page) {
+      offset = page*limit;
+      statement = statement + " OFFSET " + offset;
+    }
+
+    console.log (statement)  
+    executeQuery (statement)
+
+  });
 
 
 async function executeQuery (statement) {
